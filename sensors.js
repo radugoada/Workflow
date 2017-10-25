7bc294c081cf3dfba3c839bd0548f25dee9346ad
radugoada <32519950+radugoada@users.noreply.github.com>
2017-10-25 19:24:19 +0300

    Redenumire variabile

--- sensors.js
+++ sensors.js
@@ -4,9 +4,9 @@ window.addEventListener('devicemotion', ondevicemotion);
 
 function ondeviceorientation(event) {
 
-    document.getElementById("id_alpha").innerHTML = Math.round(event.alpha * 100) / 100;
-    document.getElementById("id_beta").innerHTML = Math.round(event.beta * 100) / 100;
-    document.getElementById("id_gamma").innerHTML = Math.round(event.gamma * 100) / 100;
+    document.getElementById("id_alpha").innerHTML = " alpha = " + Math.round(event.alpha * 100) / 100;
+    document.getElementById("id_beta").innerHTML = " beta = " + Math.round(event.beta * 100) / 100;
+    document.getElementById("id_gamma").innerHTML = " gamma = " + Math.round(event.gamma * 100) / 100;
 
 }
 
@@ -15,8 +15,8 @@ function ondevicemotion(event) {
     document.getElementById("id_acc").innerHTML = "Acc = " + Math.round(event.acceleration.x * 10) / 10+ "  " +Math.round(event.acceleration.y * 10) / 10+ " " +Math.round(event.acceleration.z * 10) / 10; 
     
     var ag = event.accelerationIncludingGravity;
-    var x_incl = Math.atan(ag.x / ag.z) * 180 / Math.PI;
-    var y_incl = Math.atan(ag.y / ag.z) * 180 / Math.PI;
-    document.getElementById("id_acc_g").innerHTML = "AccG = " + Math.round(ag.x * 10) / 10+ "  " +Math.round(ag.y * 10) / 10+ " " +Math.round(ag.z * 10) / 10 + "incl_x = " + Math.round(x_incl * 10) /10 + "incl_y = " + Math.round(y_incl * 10) / 10; 
+    var gamma = - Math.atan(ag.x / ag.z) * 180 / Math.PI;
+    var beta = Math.atan(ag.y / ag.z) * 180 / Math.PI;
+    document.getElementById("id_acc_g").innerHTML = "AccG = " + Math.round(ag.x * 10) / 10+ "  " +Math.round(ag.y * 10) / 10+ " " +Math.round(ag.z * 10) / 10 + " gamma = " + Math.round(gamma * 10) /10 + " beta = " + Math.round(beta * 10) / 10; 
     
 }
