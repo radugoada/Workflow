f0566ee1bf9b498b8c1a4c4b689c026efad5b9e0
radugoada <32519950+radugoada@users.noreply.github.com>
2017-10-25 17:43:41 +0300

    Added new sensors functionality

--- sensors.js
+++ sensors.js
@@ -1,4 +1,6 @@
+document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.10.25.0";
 window.addEventListener('deviceorientation', ondeviceorientation);
+window.addEventListener('devicemotion', ondevicemotion);
 
 function ondeviceorientation(event) {
 
@@ -6,4 +8,11 @@ function ondeviceorientation(event) {
     document.getElementById("id_beta").innerHTML = Math.round(event.beta * 100) / 100;
     document.getElementById("id_gamma").innerHTML = Math.round(event.gamma * 100) / 100;
 
+}
+
+function ondevicemovement(event) {
+ 
+    document.getElementById("id_acc").innerHTML = Math.round(event.acceleration.x * 10) / 10+ "  " +Math.round(event.acceleration.y * 10) / 10+ " " +Math.round(event.acceleration.z * 10) / 10+; 
+    document.getElementById("id_acc_g").innerHTML = Math.round(event.accelerationIncludingGravity.x * 10) / 10+ "  " +Math.round(event.accelerationIncludingGravity.y * 10) / 10+ " " +Math.round(event.accelerationIncludingGravity.z * 10) / 10+; 
+    
 }
