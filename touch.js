13cf064394f676e69114910e9487de73cb441ca9
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-08 18:16:05 +0200

    Adding random color and add variable rect

--- touch.js
+++ touch.js
@@ -1,16 +1,29 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.3";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.4";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
 canvas.addEventListener("touchstart", on_touch_start);
 
+function generate_random_color()
+{
+    var litere ="0123456789ABCDEF";
+    var color = "#";
+    for(i = 0; i < 6; i++)
+            color += litere[Math.floor(Math.random() * 16)];
+    
+    return color;
+}
+
 function on_touch_start(e)
 {
+    var rect = canvas.getBoundingClientRect();
     var touches = e.changedTouches;
-    for (i = 0; i < e.changedTouches.length; i++){
+    for (i = 0; i < e.changedTouches.length; i++)
+    {
         context.beginPath();
         context.arc(touches[i].pageX, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+        context.strokeStyle = generate_random_color();
         context.stroke();
     }
     
