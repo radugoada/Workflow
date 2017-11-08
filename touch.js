58c870dac66b97df0b21d456663dfe61c0f3fd09
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-08 17:35:46 +0200

    Create circle on touch

---
+++ touch.js
@@ -0,0 +1,17 @@
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.0";
+
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+
+windows.addEventListener("touchstart", on_touch_start);
+
+function.on_touch_start(e)
+{
+    var touches = e.changedTouches;
+    for (i = 0; i < e.changedTouches.lenght; i++){
+        context.beginPath();
+        context.arc(touches[i].pageX, touches[i].pageY, 10, 0, Math.PI);
+        context.stroke();
+    }
+    
+}
