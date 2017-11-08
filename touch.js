6bfdb7c3edd8fb2087472cece6138ac478ca184d
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-08 17:38:01 +0200

    Syntax correction

--- touch.js
+++ touch.js
@@ -3,7 +3,7 @@ document.getElementById("id_logic_level_version").innerHTML="Business level vers
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
-windows.addEventListener("touchstart", on_touch_start);
+window.addEventListener("touchstart", on_touch_start);
 
 function.on_touch_start(e)
 {
