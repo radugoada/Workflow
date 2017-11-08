cd0709ba9e6ecce4a2643015b84d316dfd3960bb
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-08 17:50:49 +0200

    Canvas touchstart and added full circle, not just half

--- touch.js
+++ touch.js
@@ -1,16 +1,16 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.0";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.2";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
-window.addEventListener("touchstart", on_touch_start);
+canvas.addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e)
 {
     var touches = e.changedTouches;
     for (i = 0; i < e.changedTouches.length; i++){
         context.beginPath();
-        context.arc(touches[i].pageX, touches[i].pageY, 10, 0, Math.PI);
+        context.arc(touches[i].pageX, touches[i].pageY, 10, 0, 2 * Math.PI);
         context.stroke();
     }
     
