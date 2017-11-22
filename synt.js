ee985e0f9122062c8e6272f1461f2271ab27ce95
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-22 18:53:07 +0200

    Bug fix

--- synt.js
+++ synt.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.0";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.1";
 
 var synt = window.speechSynthesis;
 
@@ -8,5 +8,5 @@ function get_voices()
     
     var voices = synt.getVoices();
     for (var i=0; i < voices.length; i++)
-        document.getElementById ("id_voices").innerHTML += voices[i].name + ":" voices[i].lang + "<br>";
+        document.getElementById ("id_voices").innerHTML += voices[i].name + ":" + voices[i].lang + "<br>";
 }
