966493e69795a619a44bd73e635e03b60672b035
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-22 19:06:03 +0200

    Speak input function

--- synt.js
+++ synt.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.1";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.2";
 
 var synt = window.speechSynthesis;
 
@@ -9,4 +9,14 @@ function get_voices()
     var voices = synt.getVoices();
     for (var i=0; i < voices.length; i++)
         document.getElementById ("id_voices").innerHTML += voices[i].name + ":" + voices[i].lang + "<br>";
+}
+
+function speak()
+{
+    var enunt = new SpeechSynthesisUtterance();
+    enunt.text = document.getElementById("id_text").value;
+    synt.speak(enunt);
+    
+    
+    
 }
