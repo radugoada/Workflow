9e829998b8d9ede11a496cde98091d08bfb823a4
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-22 17:55:09 +0200

    Clear rect parameters

--- canvas_move.js
+++ canvas_move.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.15.3";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.15.4";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -28,7 +28,7 @@ function on_touch_move(e)
 
 	for (var i = 0; i < touches.length; i++){
 		if (touches[i].pageX < top_x + img_width && touches[i].pageX >= top_x && touches[i].pageY < top_y + img_width && touches[i].pageY >= top_y) {
-            context.clearRect();
+            context.clearRect(0, 0, 400, 300);
             top_x = touches[i].pageX - rect_canvas.left;
             top_y = touches[i].pageY - rect_canvas.top;
             context.drawImage(img, top_x, top_y, img_width, img_height);
