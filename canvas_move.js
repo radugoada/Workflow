9ec0eb281caa81dd8385d703fa50a6513aee7f64
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-22 17:46:20 +0200

    Move image

--- canvas_move.js
+++ canvas_move.js
@@ -4,8 +4,15 @@ var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 var rect_canvas = canvas.getBoundingClientRect();
 var img = document.getElementById("id_img");
+
+var top_x = 100;
+var top_y = 100;
+
+var img_width = 100;
+var img_height = 100;
+
 img.onload = function(){
-	context.drawImage(img, 100, 100, 100, 100);
+	context.drawImage(img, top_x, top_y, img_width, img_height);
 	var rect_img = img.getBoundingClientRect();
 }
 
@@ -20,8 +27,12 @@ function on_touch_move(e)
 	var touches = e.changedTouches;
 
 	for (var i = 0; i < touches.length; i++){
-		var circle = touches[i].target;
-		
+		if (touches[i].pageX < top_x + img_width && touches[i].pageX >= top_x && touches[i].pageY < top_y + img_width && touches[i].pageY >= top_y) {
+            context.clearRect();
+            top_x = touches[i].pageX - rect_canvas.left;
+            top_y = touches[i].pageY - rect_canvas.top;
+            context.drawImage(img, top_x, top_y, img_width, img_height);
+        }
 
 	}
 
