8f184ffdfc5ca7106b11a31498d58288c6db807f
radugoada <32519950+radugoada@users.noreply.github.com>
2017-11-22 18:50:03 +0200

    Speech synthesis

---
+++ synt.js
@@ -0,0 +1,12 @@
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.0";
+
+var synt = window.speechSynthesis;
+
+
+function get_voices()
+{
+    
+    var voices = synt.getVoices();
+    for (var i=0; i < voices.length; i++)
+        document.getElementById ("id_voices").innerHTML += voices[i].name + ":" voices[i].lang + "<br>";
+}
