f4f185f0bd99c00f24453ba801d7b0a08730084d
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-10 18:01:43 +0200

    update business level

--- video.js
+++ video.js
@@ -1,6 +1,6 @@
 var d = new Date();
 
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.7" + d.getFullYear() + "." + d.getMonth() + "." + d.getDate() + ".0";
+document.getElementById("id_logic_level_version").innerHTML="Business level version" + d.getFullYear() + "." + d.getMonth() + "." + d.getDate() + ".0";
 
 var constraints = {audio:true, video:true};
 navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
