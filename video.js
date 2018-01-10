988b736960ad8de513a5fd13ee08b8b5369b10f5
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-10 17:55:36 +0200

    get media function javascript

---
+++ video.js
@@ -0,0 +1,19 @@
+var d = new Date();
+
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.7" + d.getFullYear() + "." + d.getMonth() + "." + d.getDate() + ".0";
+
+var constraints = {audio:true, video:true};
+navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
+
+
+var video = document.getElementByID("id_video");
+
+function on_success(stream)
+{
+    video.srcObject = stream;
+}
+
+function on_error(error)
+{
+    alert("Error");
+}
