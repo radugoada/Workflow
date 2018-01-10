55a87e811cb79fab96fdc9e7f21e38931d2af25d
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-10 19:06:00 +0200

    corrections

--- vibrate.js
+++ vibrate.js
@@ -1,6 +1,3 @@
-var d = new Date();
-
-document.getElementById("id_logic_level_version").innerHTML="Business level version" + d.getFullYear() + "." + d.getMonth() + "." + d.getDate() + ".1";
 
 document.getEventListener("touchstart", vibrate);
 
