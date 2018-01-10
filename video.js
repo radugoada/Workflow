7d857a4a0447ccd5a60415ee8cb848530cbc483b
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-10 18:23:21 +0200

    take picture

--- video.js
+++ video.js
@@ -7,6 +7,7 @@ navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error
 
 
 var video = document.getElementById("id_video");
+video.addEventListener("touchstart", snap);
 
 function on_success(stream)
 {
@@ -16,4 +17,12 @@ function on_success(stream)
 function on_error(error)
 {
     alert("Error");
+}
+
+function snap()
+{
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2D");
+    context.drawImage(video, 0, 0, 640, 480);
+    
 }
