5099f92e386ade57c8332f9b082309d825054a82
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-10 18:12:38 +0200

    stop audio

--- video.js
+++ video.js
@@ -2,7 +2,7 @@ var d = new Date();
 
 document.getElementById("id_logic_level_version").innerHTML="Business level version" + d.getFullYear() + "." + d.getMonth() + "." + d.getDate() + ".0";
 
-var constraints = {audio:true, video:{facingMode:"environment"}};
+var constraints = {audio:false, video:{facingMode:"environment"}};
 navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
 
 
