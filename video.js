ffaed2f3a3d56918f89e350f7b1cb761e456436c
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-10 18:10:56 +0200

    rear camera

--- video.js
+++ video.js
@@ -2,7 +2,7 @@ var d = new Date();
 
 document.getElementById("id_logic_level_version").innerHTML="Business level version" + d.getFullYear() + "." + d.getMonth() + "." + d.getDate() + ".0";
 
-var constraints = {audio:true, video:true};
+var constraints = {audio:true, video:{facingMode:"environment"}};
 navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
 
 
